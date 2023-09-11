ed5e849ffc27b0d5cf65031f7529b8a25c0bb4af
Le Trung Kien <kienle.8475@gmail.com>
2023-09-11 21:21:08 +0700

    Implement footer

---
+++ src/components/templates/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- Footer -->
+  <b-card style="background-color:rgb(0 174 246); color: white" class="font-small pt-2 mt-2">
+    <div class="footer-copyright text-center py-3">
+      <b-container fluid>
+        &copy; 2023 Copyright: <a style="color: white" href="https://www.linkedin.com/in/kienle8475/"> Kien Le - @kienle.8475@gmail.com </a>
+      </b-container>
+    </div>
+  </b-card>
+  <!-- Footer -->
+</template>
+
+<script>
+  export default {
+    name: 'footer'
+  } 
+</script>
